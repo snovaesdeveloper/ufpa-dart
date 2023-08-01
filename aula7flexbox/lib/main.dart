59b9064eac35a099341bae29423fbdc866fa72a3
Samuel <snovaes.developer@gmail.com>
2023-08-01 12:17:45 -0300

    resoluções de atividades do curso de verão aula 7 - flexbox, meiaquery

--- aula7flexbox/lib/main.dart
+++ aula7flexbox/lib/main.dart
@@ -2,24 +2,76 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MainApp());
+
 }
 
 class MainApp extends StatelessWidget {
   MainApp({super.key});
+final Map<String, List<String>> dados = {
+    'Sobremesas': [
+      'Torta de Maçã',
+      'Mousse de Chocolate',
+      'Pudim de Leite Condensado',
+    ],
+    'Pratos principais': [
+      'Frango Assado com Batatas',
+      'Espaguete à Bolonhesa',
+      'Risoto de Cogumelos',
+    ],
+    'Aperitivos': [
+      'Bolinhos de Queijo',
+      'Bruschetta de Tomate e Manjericão',
+      'Canapés de Salmão com Cream Cheese',
+    ],
+  };
+  final int? categoria = null;
 
+  
   @override
   Widget build(BuildContext context) {
+  final List<MapEntry<String, List<String>>> entries = dados.entries.toList();
+  
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Minhas receitas")),
         body: Center(
           child: SizedBox(
+            
             height: MediaQuery.of(context).size.height * 0.50,
             width: MediaQuery.of(context).size.width * 0.50,
-            child: Container(
-              color: const Color.fromARGB(255, 238, 239, 239),
-              
-            ),
+            child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          for (int i = 0; i < entries.length; i++)
+            if (categoria == null || categoria == i + 1)
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text(
+                      entries[i].key,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      for (String valor in entries[i].value)
+                        Text(
+                          valor,
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                    ],
+                  ),
+                ],
+              ),
+        ],
+      ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
